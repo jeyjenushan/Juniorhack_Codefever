4ab3764ccdd157d8f09adcf99ec83ca9f169401c
Kirisi15 <kkirisigan@gmail.com>
2025-01-25 12:11:12 +0530

    update students

--- Juniorhack_Codefever/src/main/java/com/crudoperation/Main.java
+++ Juniorhack_Codefever/src/main/java/com/crudoperation/Main.java
@@ -1,13 +1,18 @@
 package com.crudoperation;
 
+import com.crudoperation.DatabaseManagement.dbconnect;
 import com.crudoperation.Hotels.HotelManage;
 import com.crudoperation.StudentAllocation.StudentAllocationManage;
-import com.crudoperation.Students.StudentManage;
 
+import com.crudoperation.Students.StudentManagementServices;
+import com.crudoperation.Students.StudentOperation;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("----------Welcome to the Hostel Management System ---------");
         System.out.println("------ABC University");
         System.out.println("please enter the number of the action you want to proceed");
@@ -16,18 +21,30 @@ public class Main {
         System.out.println("(3) Manage Student Allocation");
         System.out.println("(4) Exit");
 
+
+            try (Connection conn = dbconnect.getConnection()) {
+                System.out.println("Connected successfully");
+
+            }
+            catch (SQLException e) {
+                System.err.println("Error occurred: " + e.getMessage());
+    }
+
         Scanner scanner= new Scanner(System.in);
         System.out.println("Please enter the number of the action you want to proceed");
         int action= scanner.nextInt();
-        switch (action){
-            case 1:
-                 HotelManage hotelManage= new HotelManage();
-            case 2:
-                StudentManage studentManage= new StudentManage();
-                case 3:
-                    StudentAllocationManage studentAllocationManage= new StudentAllocationManage();
-                    case 4:
-                        System.exit(0);
-        }
+       while(action!=4)
+       {
+           switch (action){
+               case 1:
+               case 2:
+                   StudentOperation studentOperation=StudentManagementServices.callingServices();
+                   studentOperation.operation(dbconnect.getConnection());
+               case 3:
+                   StudentAllocationManage studentAllocationManage= new StudentAllocationManage();
+               case 4:
+                   System.exit(0);
+           }
+       }
     }
 }
--- Juniorhack_Codefever/src/main/java/com/crudoperation/Students/ViewStudent.java
+++ Juniorhack_Codefever/src/main/java/com/crudoperation/Students/ViewStudent.java
@@ -1,9 +1,40 @@
 package com.crudoperation.Students;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class ViewStudent implements StudentOperation{
+    Scanner scanner=new Scanner(System.in);
 
     @Override
-    public void operation() {
+    public void operation(Connection conn) {
+        System.out.println("View Student details.../n/n");
+        System.out.println("Enter the student Id : ");
+        String id=scanner.nextLine();
+        String sql = "SELECT * FROM employees WHERE student_id = ?;";
+        StringBuilder result = new StringBuilder();
+        try(PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setString(1,id);
+            ResultSet res = preparedStatement.executeQuery();
+
+            if (res.next()){
+                String studentId = res.getString("student_id");
+                String name = res.getString("name");
+                int age = res.getInt("age");
+                String department = res.getString("department");
+                System.out.println(String.format("ID: %s, Name: %s, Age: %d, Department: %s\n", studentId, name, age, department));
+
+            } else {
+                System.out.println("employee records are empty!");
+            }
+        }
+        catch (SQLException e){
+            System.out.println("Retrieving employee is unsuccessful! Reason: "+ e.getSQLState());
+        }
+
 
     }
 }
